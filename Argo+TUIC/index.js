452e0414172b3f9d96cf711309f24e98566f8e44
EasyShare <easonhunt@protonmail.com>
2026-09-17 17:47:48 +0800

    Update index.js

--- Argo+TUIC/index.js
+++ Argo+TUIC/index.js
@@ -368,7 +368,7 @@ async function main() {
 
     try { 
       fs.writeFileSync(URL_FILE_PATH, consoleFormatted, "utf-8"); 
-      log(`[链接] Base64节点已写入: ${URL_FILE_PATH}`);
+      log(`[链接] Base64已写入: ${URL_FILE_PATH}`);
     } catch (e) {}
   };
 
