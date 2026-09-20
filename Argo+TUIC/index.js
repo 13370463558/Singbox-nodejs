3a5666a3c50be707f0e08038969bea9b72d13514
EasyShare <easonhunt@protonmail.com>
2026-09-20 15:48:18 +0800

    Update index.js

--- Argo+TUIC/index.js
+++ Argo+TUIC/index.js
@@ -37,21 +37,23 @@ const log = (msg) => process.stdout.write(msg + "\n");
 const TOTAL_MEM_MB = Math.floor(os.totalmem() / 1024 / 1024);
 
 function getGcInterval(totalMB) {
-  if (totalMB < 160) return 3 * 60 * 1000;   
-  if (totalMB < 256) return 5 * 60 * 1000;   
-  if (totalMB < 320) return 7 * 60 * 1000;  
-  if (totalMB < 448) return 10 * 60 * 1000;  
-  if (totalMB < 576) return 12 * 60 * 1000;  
-  return 15 * 60 * 1000;                     
+  if (totalMB < 160) return 8 * 60 * 1000;   
+  if (totalMB < 256) return 10 * 60 * 1000; 
+  if (totalMB < 320) return 12 * 60 * 1000; 
+  if (totalMB < 448) return 15 * 60 * 1000; 
+  if (totalMB < 576) return 15 * 60 * 1000; 
+  return 20 * 60 * 1000;                    
 }
 
 const gcIntervalMs = getGcInterval(TOTAL_MEM_MB);
 
-setInterval(() => {
-  if (typeof global.gc === "function") {
-    global.gc();
-  }
-}, gcIntervalMs);
+if (typeof global.gc === "function") {
+  setInterval(() => {
+    try {
+      global.gc();
+    } catch (e) {}
+  }, gcIntervalMs);
+}
 const iataMap = {
   HKG: "香港", TPE: "台湾", NRT: "日本", HND: "日本", KIX: "日本",
   ICN: "韩国", SIN: "新加坡", BKK: "泰国", MNL: "菲律宾", SGN: "越南",
